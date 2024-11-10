a5429832e109b632f6c62a820ff5c6863daacc8d
ibrahimghellab <ghellabibrahim@gmail.com>
2024-11-10 02:42:23 +0100

    Implementation of gemini ( if you type a message in the textBox and press enter key the AI will respond

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const key="AIzaSyAXv1l3XcVal68_fA7eeX7Jnlbrpf4pQPc";
+let URL=`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${key}`;
 
-const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
-});
+const res=document.getElementById("response");
+const input=document.getElementById("userInput");
 
-const prompt = `List a few popular cookie recipes using this JSON schema:
+const generateAPIResponse=async(messageUser)=>{
+  const response= await fetch(URL,{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({
+      contents: [{
+        role:"user",
+        parts:[{text: messageUser}]
+      }]
+      
+    })
+  });
+  const data=await response.json();
+  res.textContent=data.candidates[0].content.parts[0].text;
+ 
+}
 
-Recipe = {'recipeName': string}
-Return: Array<Recipe>`;
 
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
+
+  input.addEventListener("keydown",function(event) {
+    if(event.code=='Enter'){
+        generateAPIResponse(input.value);
+    }
+    
+  })
+
+
